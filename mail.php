07522a40081fbc9da8437c394da7f04f43c3102f
Kustiche <ChernichnyKustik@mail.ru>
2023-02-28 17:56:00 +0300

    Update 2023-02-28T14:55:57.096Z

--- mail.php
+++
@@ -1,7 +0,0 @@
-<php>
-  $to = 'ChernichnyKustik@mail.ru';
-  $tema = 'Писмо с заказом на демонтаж';
-  $message = 'Имя пользователя: '._POST['name'].'<br>';
-  $message = 'Телефон пользователя: '._POST['number'].'<br>';
-  mail($to, $tema, $message);
-</php>
